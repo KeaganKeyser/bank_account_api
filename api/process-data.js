9ab3bd714ea6164208a1d29f8ec389e8f2212d4d
KeaganKeyser <keagan.keyser@inteprit.com>
2025-12-03 20:16:28 +0530

    Update process-data.js
    #bug fix

--- api/process-data.js
+++ api/process-data.js
@@ -30,10 +30,10 @@ export default async function handler(req, res) {
     // const rawBody = await readRawBody(req);
      let outputdata;
     // const data = await req.json();
-    const match = rawBody.body.match(/```(?:json)?\s*([\s\S]*?)\s*```/i);
+    // const match = rawBody.body.match(/```(?:json)?\s*([\s\S]*?)\s*```/i);
 
     // let object = JSON.parse(rawBody);
-    // const match = object.body.match(/```(?:json)?\s*([\s\S]*?)\s*```/i);
+    const match = object.body.match(/```(?:json)?\s*([\s\S]*?)\s*```/i);
     
     
      if (match && match[1]) {
@@ -77,7 +77,7 @@ export default async function handler(req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
      // res.end(object.body);
-    res.end(outputdata);
+    res.end(match);
   } catch (err) {
     res.statusCode = 400; // Bad Request for parsing errors
     res.setHeader('Content-Type', 'application/json');
